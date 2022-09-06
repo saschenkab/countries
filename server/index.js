381e03275020bd79a851a74e298af3b44155d450
[Saschi] <saschenkagonz@outlook.com>
2022-09-06 18:15:39 -0400

    Revert "gitignore updated"
    This reverts commit 74cb5b0bd9451c399d2c71bbb815cbc953caaa33.
    
    deleted:    server/.env
    deleted:    server/index.js
    deleted:    server/package-lock.json

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const { connection } = require("./src/database");
-const server = require("./src/app");
-const port = 5000;
-
-connection.sync({ force: false }).then(() => {
-  server.listen(port, () => {
-    console.log(`Server started on port ${port}💚`);
-  });
-});
--- server/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-const countriesRoute = require("./countries");
-const router = Router();
-
-router.use("/countries", countriesRoute);
-
-module.exports = router;
