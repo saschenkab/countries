24e516e25933a03e6121b8e90839e5c220450206
[Saschi] <saschenkagonz@outlook.com>
2022-09-07 11:14:57 -0400

    sequelize models added

---
+++ server/src/models/Activity.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "activity",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      difficulty: {
+        type: DataTypes.ENUM("1", "2", "3", "4", "5"),
+        allowNull: false,
+      },
+
+      duration: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+
+      seasons: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        defaultValue: [],
+        allowNull: false,
+      },
+    },
+    {
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
+};
