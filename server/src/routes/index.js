d470989dc9ca841799d56946f6101b27a9d971c5
[Saschi] <saschenkagonz@outlook.com>
2022-09-15 14:19:36 -0400

    routes added to router middleware

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const countriesRoute = require("./countries");
+const activitiesRoute = require("./activity");
 
 const router = Router();
 
 router.use("/countries", countriesRoute);
+router.use("/activity", activitiesRoute);
 
 module.exports = router;
