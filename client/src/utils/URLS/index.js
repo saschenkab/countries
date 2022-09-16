e8886da5ac2a4d9a324aa4753faf48f5a42f12a3
[Saschi] <saschenkagonz@outlook.com>
2022-09-16 15:50:50 -0400

    urls file added

---
+++ client/src/utils/URLS/index.js
@@ -0,0 +1,3 @@
+export const GET_COUNTRIES = "http://localhost:5000/countries";
+export const COUNTRIES_BY_NAME = "http://localhost:5000/countries?name=";
+export const COUNTRIES_BY_CODE = "http://localhost:5000/countries/country/";
