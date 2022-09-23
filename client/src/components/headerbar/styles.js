2f00ed433de332921eefe9194360b6f1c2083a01
[Saschi] <saschenkagonz@outlook.com>
2022-09-22 20:26:12 -0400

    searchbar imported

--- client/src/components/headerbar/styles.js
+++ client/src/components/headerbar/styles.js
@@ -1,8 +1,4 @@
 import styled from "styled-components";
-<style>
-  @import
-  url('https://fonts.googleapis.com/css2?family=Marcellus+SC&family=Raleway:ital,wght@0,200;1,200&display=swap');
-</style>;
 
 export const Container = styled.div`
   display: flex;
@@ -19,7 +15,7 @@ export const Container = styled.div`
     text-transform: uppercase;
     text-align: center;
     margin-left: 6%;
-    color: #6f4e37;
+    color: #283618;
   }
 
   img {
@@ -38,19 +34,29 @@ export const Container = styled.div`
     }
   } ;
 `;
+
 export const Button = styled.div`
-  background-color: #bb9457;
-  color: white;
-  border-radius: 40px;
+  background-color: #606c38;
+  color: #fefae0;
+  padding: 1rem 1rem;
+  border-radius: 10px;
   font-weight: 200;
-  font-size: 2rem;
-  font-family: "Raleway", sans-serif ExtraLight 200;
+  font-size: 1.5rem;
+  font-family: "Raleway", sans-serif;
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 8px 20px;
   height: 19px;
+  box-shadow: 0 0 3px 0.1px #606c38;
+  will-change: contents;
   cursor: pointer;
+  transition: 0.3s ease-in all;
+
+  &:hover {
+    box-shadow: 0 0 5px 0.2px #606c38;
+    font-weight: 500;
+  }
+
   @media (max-width: 820px) {
     font-size: 13px;
   }
