b4edf0a9bafb060ba8ba93a1538cc8ebdae2aa6d
[Saschi] <saschenkagonz@outlook.com>
2022-09-22 20:25:23 -0400

    loader component added

---
+++ client/src/components/loader/index.jsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles";
+
+const Loader = () => {
+  return (
+    <Container>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </Container>
+  );
+};
+
+export default Loader;
---
+++ client/src/components/loader/styles.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 150px;
+  height: 70px;
+  line-height: 50px;
+  text-align: center;
+  margin: 25rem 50rem;
+
+  span {
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    background: #283618;
+    border-radius: 50px;
+    animation: up-down6 0.5s ease-in infinite alternate;
+  }
+
+  span:nth-child(2) {
+    background: #374a21;
+    animation-delay: 0.16s;
+  }
+
+  span:nth-child(3) {
+    background: #606c38;
+    animation-delay: 0.32s;
+  }
+
+  span:nth-child(4) {
+    background: #a1b076;
+    animation-delay: 0.48s;
+  }
+
+  @keyframes up-down6 {
+    0% {
+      transform: translateY(-10px);
+    }
+
+    100% {
+      transform: translateY(10px);
+    }
+  }
+`;
