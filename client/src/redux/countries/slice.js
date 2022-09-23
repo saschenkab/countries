980305bf2eb9356b1dfbac84221fb363f702c791
[Saschi] <saschenkagonz@outlook.com>
2022-09-22 20:28:18 -0400

    clean action created

--- client/src/redux/countries/slice.js
+++ client/src/redux/countries/slice.js
@@ -25,20 +25,52 @@ export const fetchCountries = createAsyncThunk("/countries", async () => {
   }
 });
 
+export const fetchCountriesByName = createAsyncThunk(
+  "/country/name",
+  async (name) => {
+    try {
+      const response = (await axios.get(COUNTRIES_BY_NAME + name)).data;
+      console.log("🚀 ~ file: slice.js ~ line 33 ~ response", response);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return error.message;
+    }
+  }
+);
+
+// export const userDiveLogList = (userId) => {
+//   return axios.get(API_URL + `userdiveloglist/${userId}`);
+// };
+
 const countriesSlice = createSlice({
   name: "countries",
   initialState,
-  reducers: {},
+  reducers: {
+    clean: {
+      reducer(state, action) {
+        state.countriesFiltered = [];
+      },
+    },
+  },
   extraReducers(builder) {
     builder
       .addCase(fetchCountries.pending, (state, action) => {
         state.status = "Loading";
-        // state.countries.push(action.payload);
+      })
+      .addCase(fetchCountriesByName.pending, (state, action) => {
+        state.status = "Loading";
       })
       .addCase(fetchCountries.fulfilled, (state, action) => {
         state.status = "Succeeded";
 
-        state.countries = action.payload;
+        state.countries = action.payload.data;
+        state.countriesFiltered = action.payload.data;
+      })
+      .addCase(fetchCountriesByName.fulfilled, (state, action) => {
+        state.status = "Succeeded";
+
+        state.countriesFiltered = action.payload.data;
       })
       .addCase(fetchCountries.rejected, (state, action) => {
         state.status = "Failed";
@@ -48,7 +80,10 @@ const countriesSlice = createSlice({
 });
 
 export const selectAllCountries = (state) => state.countries.countries;
+export const getAllCountriesFiltered = (state) =>
+  state.countries.countriesFiltered;
 export const getCountriesStatus = (state) => state.countries.status;
 export const getCountriesError = (state) => state.countries.error;
+export const { clean } = countriesSlice.actions;
 
 export default countriesSlice.reducer;
