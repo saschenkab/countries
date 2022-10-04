3894ca629e972b71a73bb0f345655baa0c2f5bf4
[Saschi] <saschenkagonz@outlook.com>
2022-10-04 15:05:38 -0400

    order component added

--- client/src/components/headerbar/index.jsx
+++ client/src/components/headerbar/index.jsx
@@ -4,7 +4,9 @@ import { selectAllCountries } from "../../redux/countries/slice";
 import map from "../../utils/photos/map.png";
 import { Button, Container } from "./styles";
 import Autocomplete from "../searchbar";
+import Order from "../order";
 import { clean } from "../../redux/countries/slice";
+
 const Header = () => {
   const dispatch = useDispatch();
   const countries = useSelector(selectAllCountries);
@@ -15,18 +17,21 @@ const Header = () => {
   const countriesAutocomplete = countries.map((country) => country.name);
 
   return (
-    <Container>
-      <Link to='/home'>
-        <Container onClick={handleChange}>
-          <img src={map} alt='' />
-          <h2>Countries</h2>
-        </Container>
-      </Link>
-      <Autocomplete suggestions={countriesAutocomplete} />
-      <Link to='/activity/addActivity'>
-        <Button>Add New Activity</Button>
-      </Link>
-    </Container>
+    <>
+      <Container>
+        <Link to='/home'>
+          <Container onClick={handleChange} active={false}>
+            <img src={map} alt='' />
+            <h2>Countries</h2>
+          </Container>
+        </Link>
+        <Autocomplete suggestions={countriesAutocomplete} />
+        <Link to='/activity/addActivity'>
+          <Button>Add New Activity</Button>
+        </Link>
+      </Container>
+      <Order />
+    </>
   );
 };
 
