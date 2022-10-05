ff8f470861cd52a067c07169412ac93e90365689
[Saschi] <saschenkagonz@outlook.com>
2022-10-05 11:50:38 -0400

    Added activity slice to reducers

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import countriesReducer from "../redux/countries/slice";
+import activitiesReducer from "../redux/activities/slice";
 
 export const store = configureStore({
   reducer: {
     countries: countriesReducer,
+    activities: activitiesReducer,
   },
 });
