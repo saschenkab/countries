42c5168d9692bc76820ea7eee4c5405317d5c199
[Saschi] <saschenkagonz@outlook.com>
2022-10-05 11:49:25 -0400

    Added missing links

--- client/src/utils/URLS/index.js
+++ client/src/utils/URLS/index.js
@@ -1,3 +1,5 @@
 export const GET_COUNTRIES = "http://localhost:5000/countries";
 export const COUNTRIES_BY_NAME = "http://localhost:5000/countries?name=";
 export const COUNTRIES_BY_CODE = "http://localhost:5000/countries/country/";
+export const GET_ACTIVITIES = "http://localhost:5000/activity/activities";
+export const POST_ACTIVITY = "http://localhost:5000/activity/add_new_activity";
